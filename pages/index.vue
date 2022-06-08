3e4a6af880aead518f433f43a1acfaa686ff4b84
Mohamed Shaaik Naseer <shaaik.naseer@mma.gov.mv>
2022-06-08 17:03:35 +0500

    Added images

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <main class="h-screen flex items-center px-6 lg:px-32 bg-purple-900 text-white">
+    <main class="h-screen flex items-center px-6 lg:px-32 text-white" style="backgroundImage: url(/images/10.jpg); background-size: cover;">
       <header class="w-full absolute left-0 top-0 p-6 lg:px-32 lg:py-16">
         <div class="flex justify-between">
           <div>
@@ -35,18 +35,16 @@
         </div>
       </header>
       <section class="w-full md:w-9/12 xl:w-8/12">
-        <span class="font-bold uppercase tracking-widest">Atomic</span>
-        <h1 class="text-3xl lg:text-5xl font-bold text-pink-500">
-          Tail<br/>Design
+        <h1 class="text-3xl lg:text-5xl font-bold text-pink-100">
+          Company Name
         </h1>
-        <p class="font-bold mb-1">The Design is in the de{Tail}s...</p>
         <p>Lorem ipsum dolor sit amet...</p>
       </section>
     </main>
 
     <div class="p-6 lg:px-32 lg:pt-32 lg:pb-0">
       <div class="w-full text-center p-6 lg:px-32 lg:py-8 mb-4 text-4xl">About Us</div>
-      <div class="w-full text-center p-6 lg:px-32 lg:py-8 bg-green-500 mt-4">
+      <div class="w-full text-center p-6 lg:px-32 lg:py-8 mt-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non enim et nulla rutrum semper sed vel tortor. Mauris gravida nulla sem. Sed nibh ligula, tristique ullamcorper rutrum nec, facilisis sit amet nulla. Maecenas in ligula ante. Donec at elementum velit. Praesent eget dui velit. Pellentesque efficitur imperdiet ipsum, nec imperdiet turpis semper quis. Integer mattis lacinia ipsum id blandit. Praesent condimentum convallis erat ac posuere.
       </div>
     </div>
@@ -54,16 +52,16 @@
     <div class="p-6 lg:px-32 lg:pt-32 lg:pb-0">
       <div class="w-full text-center p-6 lg:px-32 lg:py-8 mb-4 text-4xl">Locations</div>
       <div class="grid grid-cols-3 gap-4 text-white">
-        <div v-for="i in 9" :key="i" class="w-full bg-blue-900 h-40">0{{i}}</div>
+        <div v-for="i in 9" :key="i" class="w-full h-40" style="background-size: cover" :style="{ backgroundImage: 'url(/images/' + i + '.jpg)' }"></div>
       </div>
-      <div class="w-full text-center p-6 lg:px-32 lg:py-8 bg-red-500 mt-4">View all locations</div>
+      <div class="w-full text-center p-6 lg:px-32 lg:py-8 mt-4">View all locations</div>
     </div>
 
     <div class="p-6 lg:px-32 lg:p-32">
       <div class="w-full text-center p-6 lg:px-32 lg:py-8 mb-4 text-4xl">Contact Us</div>
-      <div class="grid grid-cols-2 gap-4 text-white">
-        <div v-for="j in 2" :key="j" class="w-full bg-blue-900">
-          <div class="w-full text-center p-6 bg-red-500 mb-4">Send us a message</div>
+      <div class="grid grid-cols-2 gap-4">
+        <div v-for="j in 2" :key="j" class="w-full">
+          <div class="w-full text-center p-6 mb-4">Send us a message</div>
           <div class="w-full text-center p-6">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non enim et nulla rutrum semper sed vel tortor. Mauris gravida nulla sem. Sed nibh ligula, tristique ullamcorper rutrum nec, facilisis sit amet nulla. Maecenas in ligula ante. Donec at elementum velit. Praesent eget dui velit. Pellentesque efficitur imperdiet ipsum, nec imperdiet turpis semper quis. Integer mattis lacinia ipsum id blandit. Praesent condimentum convallis erat ac posuere.
           </div>
@@ -75,8 +73,8 @@
       <footer class="w-full p-6 lg:p-32 bg-gray-900 text-gray-400">
         <div class="flex justify-between">
           <div>
-            <p class="font-bold mb-1">Bloop</p>
-            <p>More bloop</p>
+            <p class="font-bold mb-1">Name</p>
+            <p>Copyright</p>
           </div>
 
           <div>
